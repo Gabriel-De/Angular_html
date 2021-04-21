c8ce0970868f3ba9a3a0ffb5e02ce7a08fa3c74d
Gabriel-De <martinsgabriel50@hotmail.com>
2021-04-21 18:52:27 +0200

    Fin sección 4

---
+++ src/app/interfaces/info-pagina..interface.ts
@@ -0,0 +1,9 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  equipo_trabajo?: any[];
+  facebook?: string;
+}
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-footer',
@@ -10,7 +11,7 @@ export class FooterComponent implements OnInit {
 // Año actual que estamos
   anio: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor( public infoService: InfoPaginaService ) { }
 
   ngOnInit(): void {
   }
